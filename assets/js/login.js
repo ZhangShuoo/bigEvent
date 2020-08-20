f6b207acf3d5fc1472660644959e4827ab144764
ZhangShuoo <7954398+zhangshuo-aly@user.noreply.gitee.com>
2020-08-20 15:30:09 +0800

    完成了登录功能

---
+++ assets/js/login.js
@@ -0,0 +1,23 @@
+// ---------------------登录功能----------------------------
+$('.login form').on('submit', function (e) {
+    e.preventDefault();
+    // 收集账号密码
+    var data = $(this).serialize();
+    // ajax提交
+    $.ajax({
+        type: 'POST',
+        url: 'http://ajax.frontend.itheima.net/api/login',
+        data: data,
+        success: function (res) {
+            if (res.status === 0) {
+                // 把token保存到本地存储
+                localStorage.setItem('token', res.token);
+                // 跳转到index.html
+                location.href = '/index.html';
+            }
+        }
+    })
+})
+
+
+// ---------------------注册功能----------------------------
